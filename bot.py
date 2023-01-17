08dd1f1c54e7f8efd7292f8aa46ff96172895bee
bnvillamor <benjosephvillamor@gmial.com>
2023-01-16 22:21:29 -0800

    added answer feature, but it stops the bot(commented out at bottom)

--- bot.py
+++ bot.py
@@ -40,9 +40,8 @@ def run_bot():
         else:
             await send_message(message, user_message, is_private=False)
 
-        #if user_message[]
 
-        if message.content.startswith("!random") and message.author != client.user:
+        if message.content.startswith('!random') and message.author != client.user:
             random_messages = []
             async for msg in message.channel.history(limit=300):
                 if msg.author != client.user:
@@ -60,34 +59,14 @@ def run_bot():
             else:
                 await message.channel.send("No messages found")
 
-    '''    
-    async def get_random_message(channel, limit):
-        random_messages = []
-        last_message_timestamp = None
-        while len(random_messages) < limit:
-            messages = await channel.history(limit=min(limit, 100), before=last_message_timestamp).flatten()
-            messages = [m for m in messages if m.author != client.user]
-            random_messages.extend(messages)
-            last_message_timestamp = messages[-1].created_at
-        return random.choice(random_messages)
-
-    @client.event
-    async def on_message(message):
-        if message.content.startswith("!random") and message.author != client.user:
-            try:
-                limit = int(message.content.split(" ")[1])
-            except (IndexError, ValueError):
-                limit = 100
-            random_message = await get_random_message(message.channel, limit)
-            if len(random_message.attachments) > 0:
-                attachment = random_message.attachments[0]
-                if attachment.is_spoiler:
-                    await message.channel.send(f"Attachment was uploaded from local computer, it can't be sent again.")
-                else:
-                    await message.channel.send(file=discord.File(attachment.url))
-            else:
-                await message.channel.send(random_message.content)
         '''
+        @client.event
+        async def on_message(message):
+            if message.author == client.user:
+                return
 
-
+            if message.content.startswith('!answer'):
+                await message.channel.send(random_message.author)
+        '''
+        
     client.run(TOKEN)
